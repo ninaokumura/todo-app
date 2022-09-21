3ee0260d0d36e3e82fa12468ddcc15c48f5a5ff2
Nina Okumura <elaineokumura@gmail.com>
2022-09-21 19:10:17 +1200

    add responsive header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import moon from "./assets/images/icon-moon.svg";
+
 function App() {
   return (
-    <div className="flex min-h-screen flex-col bg-purple-800">
-      <header className="mx-auto w-full max-w-3xl">TODO</header>
+    <div className="flex min-h-screen flex-col">
+      <header className="min-h-[12rem] border bg-mobile-light bg-cover bg-no-repeat sm:min-h-[20rem] sm:bg-desktop-light">
+        <div className="center-element flex justify-between pt-12 sm:pt-20">
+          <h1 className="text-3xl font-bold tracking-[0.3em] text-very-light-gray sm:text-4xl">
+            TODO
+          </h1>
+          <img src={moon} alt="moon icon" className="h-6 w-6" />
+        </div>
+      </header>
       <main className="flex-1">
-        <div>Hello World</div>
+        <div className="center-element">Hello World</div>
       </main>
-      <footer>Made in nz</footer>
+      <footer className="center-element">Made in nz</footer>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,27 +4,31 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "josefina-sans": ['Josefin Sans', 'sans-serif']
+        "josefina-sans": ["Josefin Sans", "sans-serif"],
+      },
+      backgroundImage: {
+        "desktop-dark": "url(./assets/images/bg-desktop-dark.jpg)",
+        "mobile-dark": "url(./assets/images/bg-mobile-dark.jpg)",
+        "desktop-light": "url(./assets/images/bg-desktop-light.jpg)",
+        "mobile-light": "url(./assets/images/bg-mobile-light.jpg)",
       },
       colors: {
-        
-"bright-blue": " hsl(220, 98%, 61%)",
-"check-gradient1": "hsl(192, 100%, 67%)",
-"check-gradient2": "hsl(280, 87%, 65%)",
-"very-light-gray": "hsl(0, 0%, 98%)",
-"very-light-grayish-blue": "hsl(236, 33%, 92%)",
-"light-grayish-blue": "hsl(233, 11%, 84%)",
-"dark-grayish-blue": "hsl(236, 9%, 61%)",
-"very-dark-grayish-blue": "hsl(235, 19%, 35%)",
-"very-dark-blue": "hsl(235, 21%, 11%)",
-"very-dark-desaturated-blue": "hsl(235, 24%, 19%)",
-"light-grayish-blue": "hsl(234, 39%, 85%)",
-"light-grayish-blue-hover": "hsl(236, 33%, 92%)",
-"dark-grayish-blue": "hsl(234, 11%, 52%)",
-"very-dark-grayish-blue": "hsl(233, 14%, 35%)",
-"very-dark-grayish-blue": "hsl(237, 14%, 26%)",
-
-      }
+        "bright-blue": " hsl(220, 98%, 61%)",
+        "check-gradient1": "hsl(192, 100%, 67%)",
+        "check-gradient2": "hsl(280, 87%, 65%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+        "very-light-grayish-blue": "hsl(236, 33%, 92%)",
+        "light-grayish-blue": "hsl(233, 11%, 84%)",
+        "dark-grayish-blue": "hsl(236, 9%, 61%)",
+        "very-dark-grayish-blue": "hsl(235, 19%, 35%)",
+        "very-dark-blue": "hsl(235, 21%, 11%)",
+        "very-dark-desaturated-blue": "hsl(235, 24%, 19%)",
+        "light-grayish-blue": "hsl(234, 39%, 85%)",
+        "light-grayish-blue-hover": "hsl(236, 33%, 92%)",
+        "dark-grayish-blue": "hsl(234, 11%, 52%)",
+        "very-dark-grayish-blue": "hsl(233, 14%, 35%)",
+        "very-dark-grayish-blue": "hsl(237, 14%, 26%)",
+      },
     },
   },
   plugins: [],
