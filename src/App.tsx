eafc2bc453ae3e2728c828f6d39729770764e10b
Nina Okumura <elaineokumura@gmail.com>
2022-10-06 18:14:34 +1300

    save todo on local storage + fix getNextTodoId function

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { ChangeEvent, KeyboardEvent, SyntheticEvent, useState } from "react";
+import {
+  ChangeEvent,
+  KeyboardEvent,
+  SyntheticEvent,
+  useState,
+  useEffect,
+} from "react";
 import Checkbox from "./components/Checkbox";
 import Input from "./components/Input";
 import TodoList from "./components/TodoList";
 import ToggleDarkMode from "./components/ToggleDarkMode";
+import tw from "tailwind-styled-components";
 
 export type TodoStatus = "completed" | "pending";
 
@@ -12,14 +19,39 @@ export type TodoItem = {
   status: TodoStatus;
 };
 
+const Header = tw.header`
+ relative min-h-[12rem] bg-mobile-light bg-cover bg-no-repeat  dark:bg-mobile-dark sm:min-h-[20rem] sm:bg-desktop-light dark:sm:bg-desktop-dark
+`;
+
 function App() {
   const [todos, setTodos] = useState<TodoItem[]>([]);
   const [newTodo, setNewTodo] = useState("");
   const [checkedAll, setCheckedAll] = useState(false);
 
+  // set item on local storage
+  useEffect(() => {
+    // convert to a string
+    if (todos.length > 0) {
+      window.localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  }, [todos]);
+
+  // get items on local storage
+  useEffect(() => {
+    const data = window.localStorage.getItem("todos");
+    console.log(data);
+    if (!data) {
+      return;
+    }
+    // convert to object
+    setTodos(JSON.parse(data));
+  }, []);
+
   // function that returns next id
   const getNextTodoId = () => {
-    return String(todos.length + 1);
+    const ids = todos.map(todo => Number(todo.id));
+    const maxId = Math.max(...ids);
+    return String(ids.length ? maxId + 1 : 1);
   };
 
   // track todo input value
@@ -30,11 +62,13 @@ function App() {
   // add todos
   const handleAddTodo = (evt: KeyboardEvent<HTMLInputElement>) => {
     if (evt.key === "Enter") {
+      const nextTodoID = getNextTodoId();
+      console.log(nextTodoID);
       setTodos([
         ...todos,
         {
           text: newTodo,
-          id: getNextTodoId(),
+          id: nextTodoID,
           status: "pending",
         },
       ]);
@@ -54,18 +88,18 @@ function App() {
 
   return (
     <div className="flex min-h-screen flex-col bg-very-light-gray font-josefina-sans dark:bg-very-dark-blue">
-      <header className="relative min-h-[12rem] bg-mobile-light bg-cover bg-no-repeat  dark:bg-mobile-dark sm:min-h-[20rem] sm:bg-desktop-light dark:sm:bg-desktop-dark">
+      <Header>
         <div className="center-element flex justify-between pt-12 sm:pt-20">
           <h1 className="text-3xl font-bold tracking-[0.3em] text-very-light-gray sm:text-4xl">
             TODO
           </h1>
           <ToggleDarkMode />
         </div>
-      </header>
+      </Header>
       <main className="flex-1">
         {/* Enter todo */}
         <div className="absolute top-24 right-0 left-0 mx-auto px-3 sm:top-56">
-          <div className="center-element relative h-14 overflow-hidden rounded-md bg-very-light-gray text-sm dark:text-dark-grayish-blue">
+          <div className="center-element relative h-14 overflow-hidden rounded-md text-sm dark:text-dark-grayish-blue">
             <Input
               onKeyDown={handleAddTodo}
               placeholder="Create a new todo..."
